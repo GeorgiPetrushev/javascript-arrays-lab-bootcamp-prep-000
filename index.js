653191c756c75989da75617c3560e915e5d379fa
Georgi Petrushev <figoto660@gmail.com>
2019-08-24 20:54:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,10 @@ function destructivelyRemoveFirstKitten(){
 function prependKitten(name){
     return [name, ...kittens];
 }
+function removeLastKitten(){
+    return kittens.slice(0,kittens.length-1)
+}
+
+function removeFirstKitten(){
+    return kittens.slice(1)
+}
